c93d15875eea2a7e4499499557c1f55f3a2ce296
Parth <parthbatwara@gmail.com>
2023-10-28 23:38:52 +0530

    random

--- timer_backend/timer/views.py
+++ timer_backend/timer/views.py
@@ -33,7 +33,7 @@ def time(request,pk):
         return Response(serializer.data)
 
     if request.method == "PUT":
-        serializer = TimerSerializer(time, data=request.data)
+        serializer = TimerSerializer(1, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
