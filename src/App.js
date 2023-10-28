21c698751ea9e5846c1db38d7065815677f0c851
Parth <parthbatwara@gmail.com>
2023-10-28 16:30:27 +0530

    Frontend API added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
+// import "./App.css";
+// import CountdownTimer from "./Components/Countdown Timer/CountdownTimer";
+// import Header from "./Components/Page/Header";
+
+// function App() {
+//   let timer = Date.now();
+//   fetch("http://127.0.0.1:8000/timer/1")
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data.timer);
+//       let timeTaken = data.timer;
+//       timer += timeTaken * 1000 * 60;
+//     })
+//     .catch((error) => {
+//       console.error("There was a problem with the fetch operation:", error);
+//     });
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <CountdownTimer countdownTimestampMs={timer} />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CountdownTimer from "./Components/Countdown Timer/CountdownTimer";
 import Header from "./Components/Page/Header";
-import WebSocket from "websocket";
 
 function App() {
-  const [timeTaken, setTimeTaken] = useState(0);
+  const [timer, setTimer] = useState(Date.now());
 
-  let timer = Date.now() + timeTaken * 1000 * 60;
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/timer/1")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.timer);
+        let timeTaken = data.timer;
+        setTimer(timer + timeTaken * 1000 * 60);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []); // Empty dependency array, runs the effect once on mount
+
   return (
     <div className="App">
       <Header />
